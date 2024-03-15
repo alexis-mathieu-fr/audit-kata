c4df3ee0bed441522b3eab5053efef37590082b2
Alexis MATHIEU <alexis.mathieu.p@citeo.com>
2024-03-15 12:18:00 +0100

    suppression de filesystem-ajout de la classe fileContent-Modification dans la classe AuditManager

--- typescript/AuditManager.ts
+++ typescript/AuditManager.ts
@@ -1,17 +1,19 @@
-﻿export class AuditManager {
+﻿import {FileContent} from "./FileContent";
+
+export class AuditManager {
     maxEntriesPerFile: number
     directoryName: string
-    fileSystem: IFileSystem
+    filesContent: FileContent[]
 
-    constructor(maxEntriesPerFile: number, directoryName: string, fileSystem: IFileSystem) {
+    constructor(maxEntriesPerFile: number, directoryName: string, filesContent: FileContent[]) {
         this.maxEntriesPerFile = maxEntriesPerFile
         this.directoryName = directoryName
-        this.fileSystem = fileSystem
+        this.filesContent = filesContent
     }
 
     addRecord(visitorName: string, timeOfVisit: Date) {
-        let filePaths = this.fileSystem.getFiles(this.directoryName)
-        let sorted = this.sortByIndex(filePaths)
+        //let filePaths = this.fileSystem.getFiles(this.directoryName)
+        let sorted = this.sortByIndex(this.filesContent)
         let newRecord = `${visitorName};${this.formatDate(timeOfVisit)}`
 
         if (sorted.length == 0) {
@@ -21,23 +23,23 @@
         }
 
         let last = sorted[sorted.length - 1]
-        let lines = this.fileSystem.readAllLines(last.path)
+        let lines = this.fileSystem.readAllLines(last.fileContent)
 
         if (lines.length < this.maxEntriesPerFile) {
             lines.push(newRecord)
             let newContent = `\n${lines}`
-            this.fileSystem.writeAllText(last.path, newContent)
+            this.fileSystem.writeAllText(last.fileContent, newContent)
         } else {
-            const newIndex = last.index + 1;
-            const newName = `audit_${newIndex}.txt`;
+            const newIndex = last.index + 1
+            const newName = `audit_${newIndex}.txt`
             const newFile = `${this.directoryName}/${newName}`
-            this.fileSystem.writeAllText(newFile, newRecord);
+            this.fileSystem.writeAllText(newFile, newRecord)
         }
     }
 
-    sortByIndex(filePaths: string[]): { index: number, path: string }[] {
-        return filePaths.sort()
-            .map((path: string, index: number) => ({index: index + 1, path: path}))
+    sortByIndex(fileContents: FileContent[]): { index: number, fileContent: FileContent }[] {
+        return fileContents.toSorted()
+            .map((fileContent, index) => ({index: index + 1, fileContent}))
     }
 
     formatDate(date: Date): string {
@@ -49,7 +51,7 @@
         const minutes = this.formatToTwoDigits(date.getMinutes())
         const seconds = this.formatToTwoDigits(date.getSeconds())
 
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
     }
 
     formatToTwoDigits(n: any): string {
---
+++ typescript/FileContent.ts
@@ -0,0 +1,6 @@
+export class FileContent {
+    
+    constructor(public readonly name: string, public readonly lines: string[]) {
+        
+    }
+}
